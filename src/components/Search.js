93d150d3da0ec0bdf00002e8eb0f0f180e9e685a
ymchoi <ymchoi@wemb.co.kr>
2020-02-18 07:43:31 +0900

    created Search Component

---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Search = (props) => {
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleSearchInputChanges = (e) => {
+        setSearchValue(e.target.value)
+    }
+
+    const callSearchFunction = (e) => {
+        e.preventDefault();
+        props.search(searchValue);
+        resetInputField;
+    }
+
+    const resetInputField = () => {
+        setSearchValue("")
+    }
+
+    return (
+        <form className="search">
+            <input
+                value={searchValue}
+                onChange={handleSearchInputChanges}
+                type="text"
+            />
+            <input onCilci={callSearchFunction} type="submit" value="SEARCH"/>
+        </form>
+    );
+};
+
+export default Search;
